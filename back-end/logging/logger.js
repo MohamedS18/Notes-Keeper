6282ed34922268b9deac68c60a1682d9ac7a2cb5
MOHAMED <mohamedsaajid18s@gmail.com>
2025-02-23 00:30:39 +0530

    production ready final

--- back-end/logging/logger.js
+++ back-end/logging/logger.js
@@ -1,8 +1,5 @@
-const fs = require("fs");
-const path = require("path");
 const moment = require("moment");
 
-const logFilePath = path.join(__dirname, "logs.txt");
 
 function logger (method, name){
     const logEntry = `[${moment().format("DD-MM-YYYY HH:mm:ss")}]  ${method} -user:${!name ? "Unknown" : name.substring(0,4)}\n`; 
