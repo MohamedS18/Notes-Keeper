818ae95aa3c26c502e44221f849105f7db43fc3a
MOHAMED <mohamedsaajid18s@gmail.com>
2025-02-22 21:40:39 +0530

    base change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Fix asset loading issue
+  base: "./",
   build: {
-    outDir: 'dist',  // Ensure output directory is 'dist'
+    outDir: 'dist',
   },
 });
